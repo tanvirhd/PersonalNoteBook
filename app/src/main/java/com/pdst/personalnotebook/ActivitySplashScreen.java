28650099f964abb4179bd63c932be24c5e9ca777
tanvirhd <tanvirahc15@gmail.com>
2021-08-10 22:54:10 +0600

    SplashScreen Activity Added

---
+++ app/src/main/java/com/pdst/personalnotebook/ActivitySplashScreen.java
@@ -0,0 +1,14 @@
+package com.pdst.personalnotebook;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivitySplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+}
