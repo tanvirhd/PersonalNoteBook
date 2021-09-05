17dad063019afb5915a5afe39b3f70b8358097fd
tanvirhd <tanvirahc15@gmail.com>
2021-09-05 22:16:45 +0600

    lockscreen activity added+splash screen ui done

---
+++ app/src/main/java/com/pdst/personalnotebook/ui/ActivityLockScreen.java
@@ -0,0 +1,16 @@
+package com.pdst.personalnotebook.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.pdst.personalnotebook.R;
+
+public class ActivityLockScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lock_screen);
+    }
+}
--- app/src/main/java/com/pdst/personalnotebook/ui/ActivitySplashScreen.java
+++ app/src/main/java/com/pdst/personalnotebook/ui/ActivitySplashScreen.java
@@ -2,15 +2,26 @@ package com.pdst.personalnotebook.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 
 import com.pdst.personalnotebook.R;
 
 public class ActivitySplashScreen extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(ActivitySplashScreen.this,ActivityHome.class));
+            }
+        }, 2000);
     }
 }
