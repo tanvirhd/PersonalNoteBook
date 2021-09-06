3cd176fb205cd1878047b4b03655653c18a3e950
Partha Pratim Mallick <parthamallick46@gmail.com>
2021-09-06 23:14:41 +0600

    home screen Activity

--- app/src/main/java/com/pdst/personalnotebook/model/ModelNote.java
+++ app/src/main/java/com/pdst/personalnotebook/model/ModelNote.java
@@ -7,7 +7,7 @@ public class ModelNote {
     private String noteBody;
     private String modifiedOn;
     private int categoryId;
-
+     
     private boolean isPinned;
     private boolean hasAttachment;
     private List<String> attachmentPath;
@@ -28,6 +28,78 @@ public class ModelNote {
         this.hasAttachment = hasAttachment;
         this.attachmentPath = attachmentPath;
         this.isProtected = isProtected;
+        this.notePassword = notePassword;
+    }
+
+    public String getNoteTitle() {
+        return noteTitle;
+    }
+
+    public void setNoteTitle(String noteTitle) {
+        this.noteTitle = noteTitle;
+    }
+
+    public String getNoteBody() {
+        return noteBody;
+    }
+
+    public void setNoteBody(String noteBody) {
+        this.noteBody = noteBody;
+    }
+
+    public String getModifiedOn() {
+        return modifiedOn;
+    }
+
+    public void setModifiedOn(String modifiedOn) {
+        this.modifiedOn = modifiedOn;
+    }
+
+    public int getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(int categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public boolean isPinned() {
+        return isPinned;
+    }
+
+    public void setPinned(boolean pinned) {
+        isPinned = pinned;
+    }
+
+    public boolean isHasAttachment() {
+        return hasAttachment;
+    }
+
+    public void setHasAttachment(boolean hasAttachment) {
+        this.hasAttachment = hasAttachment;
+    }
+
+    public List<String> getAttachmentPath() {
+        return attachmentPath;
+    }
+
+    public void setAttachmentPath(List<String> attachmentPath) {
+        this.attachmentPath = attachmentPath;
+    }
+
+    public boolean isProtected() {
+        return isProtected;
+    }
+
+    public void setProtected(boolean aProtected) {
+        isProtected = aProtected;
+    }
+
+    public String getNotePassword() {
+        return notePassword;
+    }
+
+    public void setNotePassword(String notePassword) {
         this.notePassword = notePassword;
     }
 }
