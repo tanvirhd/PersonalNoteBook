8c6865f658c80d6b7531a0700896ea4d0b6f547a
adib3006 <ma.adib3006@gmail.com>
2021-09-24 22:18:24 +0600

    added create note UI

--- app/src/main/java/com/pdst/personalnotebook/ui/ActivityNote.java
+++ app/src/main/java/com/pdst/personalnotebook/ui/ActivityNote.java
@@ -3,14 +3,24 @@ package com.pdst.personalnotebook.ui;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.pdst.personalnotebook.R;
 
 public class ActivityNote extends AppCompatActivity {
 
+    Spinner spinner = (Spinner) findViewById(R.id.spinner);
+    ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+            R.array.category, android.R.layout.simple_spinner_item);
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_note);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
     }
 }
